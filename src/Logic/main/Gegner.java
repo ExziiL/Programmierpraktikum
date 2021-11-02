bd3a68bca173b3b521d5d67c68bbeb00d43796b8
Johannes <johapr01@gmail.com>
2021-11-02 16:04:47 +0100

    Klassen hinzugefügt

---
+++ src/Logic/main/Gegner.java
@@ -0,0 +1,19 @@
+package main;
+import java.awt.*;
+
+public class Gegner {
+
+    private int[][] spielarray;
+
+    // public Point feuern(){}
+
+    /*public Point getroffen(){
+        switch (spielarray[""][""]){
+            case 0: System.out.println("Verfehlt!");break;
+            case 1: System.out.println("Treffer!");break;
+            case 2: System.out.println("Versenkt!!!");break;
+            default: break;
+        }
+        ;
+    }*/
+}
---
+++ src/Logic/main/Schiffe.java
@@ -0,0 +1,5 @@
+package main;
+
+public class Schiffe {
+    protected int groesse;
+}
