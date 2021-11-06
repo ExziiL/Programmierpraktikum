19a370b007d2de224b57e85561732aa36823f61d
Johannes <johapr01@gmail.com>
2021-11-06 13:06:07 +0100

    Methoden erstellt

--- src/Logic/Controller.java
+++ src/Logic/Controller.java
@@ -1,4 +1,24 @@
 package Logic;
+import Logic.Spielfeld.*;
+import java.io.File;
 
 public class Controller {
+
+    public void spielStarten(){
+        //initialiesierungSpielfeld();
+        //erstelleSpieler();
+    }
+
+    public void spielLaden(){
+
+    }
+
+    public void einstellungenOeffnen(){
+
+    }
+
+    public void spielBeenden(){
+        System.exit(0);
+    }
+
 }
