fa8c3da469dc69dde0fb2fb8bb27fe0d5fb8825e
unknown <daniel-stuckenberger@web.de>
2021-11-10 12:18:58 +0100

    Controller

--- src/Logic/main/Controller.java
+++ src/Logic/main/Controller.java
@@ -1,4 +1,20 @@
 package Logic.main;
 
 public class Controller {
+    public Controller() {
+    }
+
+    public void SpielLaden() {
+
+    }
+
+    public void SpielStarten() {
+
+    }
+
+    public void SpielSpeichern() {
+    }
+
+    public void Einstellungen() {
+    }
 }
