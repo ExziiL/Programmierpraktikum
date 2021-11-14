d89367a37650cf5d9b0b66f219d5673ec15ebf28
unknown <daniel-stuckenberger@web.de>
2021-11-14 14:30:19 +0100

    Wechsel zwischen Spieleinstellungen und zurück

--- src/GUI/Controller/AppController.java
+++ src/GUI/Controller/AppController.java
@@ -1,16 +1,5 @@
 package GUI.Controller;
 
-import GUI.App;
-import com.sun.javafx.scene.traversal.ParentTraversalEngine;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
 public class AppController {
 
 
--- src/GUI/Controller/SpieleinstellungenController.java
+++ src/GUI/Controller/SpieleinstellungenController.java
@@ -1,4 +1,20 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import GUI.App;
+
+import java.io.IOException;
+
 public class SpieleinstellungenController {
+    private App app;
+
+    @FXML
+    private Button SpielStarten;
+
+    @FXML
+    void zurueck(ActionEvent event) throws IOException {
+        app.zeigeSpielStarten();
+    }
 }
