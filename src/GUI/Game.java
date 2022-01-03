f0f8789f9b14ca83dbd76d75ec676486c075f3da
unknown <daniel-stuckenberger@web.de>
2022-01-03 14:32:16 +0100

    Rezising is now possible

--- src/GUI/Game.java
+++ src/GUI/Game.java
@@ -2,31 +2,26 @@ package GUI;
 
 import Logic.main.Controller;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Game extends Application {
-    private Stage primaryStage;
-    private static BorderPane mainLayout;
     public static Controller logicController;
+    private static BorderPane mainLayout;
+    private Stage primaryStage;
 
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle(GUIConstants.titel);
-        showAppWindow();
-        showStartGameWindow();
-        logicController = new Controller();
+    public static void main(String[] args) {
+        launch(args);
     }
 
     //     this.primaryStage.widthProperty().addListener(new ChangeListener<Number>() {
@@ -55,16 +50,13 @@ public class Game extends Application {
     //     });
     // }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     public static void showStartGameWindow() throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Game.class.getResource("StartGame/StartGame.fxml"));
         BorderPane startGame = loader.load();
         mainLayout.setCenter(startGame);
         mainLayout.setStyle("-fx-background-color: #ffffff");
+
     }
 
     public static void showGameSettingsWindow() throws IOException {
@@ -88,15 +80,81 @@ public class Game extends Application {
         mainLayout.setCenter(playingField);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle(GUIConstants.titel);
+        showAppWindow();
+        showStartGameWindow();
+        logicController = new Controller();
+
+    }
+
     private void showAppWindow() throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Game.class.getResource("Game.fxml"));
         // Parent root = FXMLLoader.load(getClass().getResource("Game.fxml"));
 
         mainLayout = loader.load();
-        Scene scene = new Scene(mainLayout, 800, 600);
+        Scene scene = new Scene(new Group(mainLayout));
         this.primaryStage.setScene(scene);
         this.primaryStage.show();
+
+        letterbox(scene, mainLayout);
     }
 
+
+    private void letterbox(final Scene scene, final Pane contentPane) {
+        final double initWidth = scene.getWidth();
+        final double initHeight = scene.getHeight();
+        final double ratio = initWidth / initHeight;
+
+        SceneSizeChangeListener sizeListener = new SceneSizeChangeListener(scene, ratio, initHeight, initWidth, contentPane);
+        scene.widthProperty().addListener(sizeListener);
+        scene.heightProperty().addListener(sizeListener);
+    }
+
+
+    private static class SceneSizeChangeListener implements ChangeListener<Number> {
+        private final Scene scene;
+        private final double ratio;
+        private final double initHeight;
+        private final double initWidth;
+        private final Pane contentPane;
+
+        public SceneSizeChangeListener(Scene scene, double ratio, double initHeight, double initWidth, Pane contentPane) {
+            this.scene = scene;
+            this.ratio = ratio;
+            this.initHeight = initHeight;
+            this.initWidth = initWidth;
+            this.contentPane = contentPane;
+        }
+
+        @Override
+        public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+            final double newWidth = scene.getWidth();
+            final double newHeight = scene.getHeight();
+
+            double scaleFactor =
+                    newWidth / newHeight > ratio
+                            ? newHeight / initHeight
+                            : newWidth / initWidth;
+
+            if (scaleFactor >= 1) {
+                Scale scale = new Scale(scaleFactor, scaleFactor);
+                scale.setPivotX(0);
+                scale.setPivotY(0);
+                scene.getRoot().getTransforms().setAll(scale);
+
+                contentPane.setPrefWidth(newWidth / scaleFactor);
+                contentPane.setPrefHeight(newHeight / scaleFactor);
+            } else {
+                contentPane.setPrefWidth(Math.max(initWidth, newWidth));
+                contentPane.setPrefHeight(Math.max(initHeight, newHeight));
+            }
+        }
+    }
 }
+
+
+
